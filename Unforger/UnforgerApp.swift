d7ddb9ff38a86a2d8d494993ee66afb36fa4f153
nick <nsulistio@student.ciputra.ac.ic>
2023-05-26 08:19:47 +0700

    Initial Commit

---
+++ Unforger/UnforgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnforgerApp.swift
+//  Unforger
+//
+//  Created by MacBook Pro on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct UnforgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
