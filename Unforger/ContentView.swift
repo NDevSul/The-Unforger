8f514a931429d5bb9e06f01842ce00af38cc71ee
JasonEmanuel <jemanuel@student.ciputra.ac.id>
2023-06-01 14:02:04 +0700

    Halo Nic

--- Unforger/ContentView.swift
+++ Unforger/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, Apa Kabar Nick Ganteng")
         }
         .padding()
     }
