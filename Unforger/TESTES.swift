9cab01463028f6e8a93816b524650717df7013b6
nick <nsulistio@student.ciputra.ac.ic>
2023-06-01 13:39:40 +0700

    TEST

---
+++ Unforger/TESTES.swift
@@ -0,0 +1,20 @@
+//
+//  TESTES.swift
+//  Unforger
+//
+//  Created by MacBook Pro on 01/06/23.
+//
+
+import SwiftUI
+
+struct TESTES: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TESTES_Previews: PreviewProvider {
+    static var previews: some View {
+        TESTES()
+    }
+}
