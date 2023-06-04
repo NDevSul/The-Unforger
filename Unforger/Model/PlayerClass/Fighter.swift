89b570f0fbb153050a0120480cf974a96f7fac44
nick <nsulistio@student.ciputra.ac.ic>
2023-06-04 23:27:33 +0700

    Add a fighter to the game, (animation included )

--- Unforger/Model/PlayerClass/Fighter.swift
+++ Unforger/Model/PlayerClass/Fighter.swift
@@ -1,5 +1,5 @@
 //
-//  Mage.swift
+//  Fighter.swift
 //  Unforger
 //
 //  Created by MacBook Pro on 04/06/23.
@@ -7,12 +7,12 @@
 
 import Foundation
 
-struct Mage: Classable {
+struct Fighter: Classable {
     var damageAnimationCount: Int = 3
     var idleAnimationCount: Int = 7
     var attackAnimationCount: Int = 4
     var dyingAnimationCount: Int = 6
-    var type: String = "Mage"
+    var type: String = "Fighter"
     
     var skills: [Skills] = [
         Skills(skillname: "Booty shaker", skilldamage: 100, skillmana: 1)
