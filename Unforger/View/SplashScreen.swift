3dd091f8197c30a076f5ca28cd0869d332cf496f
Devano <danggara@student.ciputra.ac.id>
2023-06-05 00:53:22 +0700

    tes

--- Unforger/View/SplashScreen.swift
+++ Unforger/View/SplashScreen.swift
@@ -17,24 +17,7 @@ struct SplashScreen: View {
         if isActive {
             ContentView()
         } else {
-            VStack {
-                VStack {
-                    Image("The_Unforger")
-                        .font(.system(size: 80))
-                        .foregroundColor(.red)
-                    Text("")
-                        .font(Font.custom("Baskerville-Bold", size: 26))
-                        .foregroundColor(.black.opacity(0.80))
-                }
-                .scaleEffect(size)
-                .opacity(opacity)
-                .onAppear {
-                    withAnimation(.easeIn(duration: 1.2)) {
-                        self.size = 0.9
-                        self.opacity = 1.00
-                    }
-                }
-            }
+            
             .onAppear {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
                     withAnimation {
